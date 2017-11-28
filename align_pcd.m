7dc6c804e2b5e2e71baaf06577e47c1aa9e6429c
Notion33 <notion33@gmail.com>
2017-11-28 13:23:54 +0900

    1st max, 2nd max 내부 height 점들만 따기(top & bottom cut)

--- align_pcd.m
+++ align_pcd.m
@@ -1,33 +1,36 @@
-%%초기 변수 선언
-%%=========================================================================
+%% Initializing
 
-wallLayerHeight = 3;
+Wall_Offset = 0.3;
 scale = 16;
-
-%%=========================================================================
-
-
 ptCloud = pcread('j_engineering_all.pcd');
 
+%% XY-plane에 PCD Align 시키기
+
+%plane fit
 XYZ=ptCloud.Location;
-%%plane fit
 c=mean(XYZ,1);
 Pc=bsxfun(@minus,XYZ,c);    
 [~,~,V]=svd(Pc,0);
-n=V(:,end);  %normal
-n=n*sign(dot(n,[0,0,1])); %orientation convention
+
+%normal벡터 계산
+n=V(:,end);
+
+%orientation convention
+n=n*sign(dot(n,[0,0,1]));
+
 %rotate/align with xy-plane
 u=cross(n,[0,0,1]);
 deg=acosd(dot(n,[0,0,1]));
 XYZnew=AxelRot(XYZ.',deg,u,[0,0,0]).';
 ptAlign = pointCloud(XYZnew);
+
 % figure
 % pcshow(ptAlign);
 
 % https://kr.mathworks.com/matlabcentral/answers/232828-how-to-fit-a-plane-to-my-point-cloud-data-and-rotate-it-so-that-the-plane-is-parallel-to-the-x-y-pla
 % https://kr.mathworks.com/matlabcentral/fileexchange/30864-3d-rotation-about-shifted-axis
 
-%===============================================Wall height extraction
+%% Wall height extraction
 
 height_start = floor(ptAlign.ZLimits(1)*10)/10;
 height_end = floor(ptAlign.ZLimits(2)*10)/10;
@@ -40,52 +43,53 @@ for i = 1 : ptAlign.Count
     height_count(line_1, 2) = height_count(line_1, 2)+1;
 end
 
-% [temp,maxHeight] = max(height_count);
-% pointWall = [];
-% 
-% wallLayerHeight = abs((wallLayerHeight + 1)/2);
-% 
-% for i = 1 : ptCloudB.Count
-%     if floor(ptCloudB.Location(i,3)) - height_start + 1 > maxHeight(2) - wallLayerHeight && floor(ptCloudB.Location(i,3)) - height_start + 1 < maxHeight(2) + wallLayerHeight
-%         pointWall = [pointWall; ptCloudB.Location(i,1:3)];
-%     end
-% end
-% ptWall = pointCloud(pointWall);
-% 
+[temp,first_maxHeight] = max(height_count);                                     % Z height내 첫번째 두번째 최댓값 찾기
+height_count(first_maxHeight(2),:) = [];
+[temp,second_maxHeight] = max(height_count);
+height_count(second_maxHeight(2),:) = [];
+
+pointWall = [];
+
+for i = 1 : ptCloudB.Count
+    if floor(ptCloudB.Location(i,3)) - height_start > first_maxHeight(2)/10 + Wall_Offset && floor(ptCloudB.Location(i,3)) - height_start < second_maxHeight(2)/10 - Wall_Offset
+        pointWall = [pointWall; ptCloudB.Location(i,1:3)];
+    end
+end
+ptWall = pointCloud(pointWall);
 % figure
 % pcshow(ptWall);
 
-%===============================================2D Projection
+%% 2D Projection
 
-% px=pointWall(:,1);
-% py=pointWall(:,2);
-% 
-% Ir = max(py)-min(py);
-% Ic = max(px)-min(px);
-% numr = round(Ir * scale);
-% numc = round(Ic * scale);
-% 
-% fprintf('2. pt2image start (2/4)\n')
-% % grid construction
-% tol = 10*2;
-% xl = min(px); xr = max(px); yl = min(py); yr = max(py);
-% xx = linspace(xl,xr,numc); yy = linspace(yl,yr,numr);
-% [X,Y] = meshgrid(xx,yy);
-% grid_centers = [X(:),Y(:)];
-% 
-% % classification
-% class = knnsearch(grid_centers,[px,py]); 
-% 
-% % data_grouping
-% class_stat = zeros(numr*numc, 1);
-% class_stat(class) = 1;
-% 
-% % 2D reshaping
-% class_stat_M = reshape(class_stat, size(X)); 
-% img = 1-class_stat_M(end:-1:1,:);
-% 
-% rstimg = ones(numr+tol, numc+tol);
-% rstimg((tol/2)+1:(tol/2)+numr,(tol/2)+1:(tol/2)+numc) = img;
-% 
-% figure
-% imshow(rstimg)
+px=pointWall(:,1);
+py=pointWall(:,2);
+
+Ir = max(py)-min(py);
+Ic = max(px)-min(px);
+numr = round(Ir * scale);
+numc = round(Ic * scale);
+
+fprintf('2. pt2image start (2/4)\n')
+% grid construction
+tol = 10*2;
+xl = min(px); xr = max(px); yl = min(py); yr = max(py);
+xx = linspace(xl,xr,numc); yy = linspace(yl,yr,numr);
+[X,Y] = meshgrid(xx,yy);
+grid_centers = [X(:),Y(:)];
+
+% classification
+class = knnsearch(grid_centers,[px,py]); 
+
+% data_grouping
+class_stat = zeros(numr*numc, 1);
+class_stat(class) = 1;
+
+% 2D reshaping
+class_stat_M = reshape(class_stat, size(X)); 
+img = 1-class_stat_M(end:-1:1,:);
+
+rstimg = ones(numr+tol, numc+tol);
+rstimg((tol/2)+1:(tol/2)+numr,(tol/2)+1:(tol/2)+numc) = img;
+
+figure
+imshow(rstimg)
