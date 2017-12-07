c0550a33b2530fcc83482db4af94ee11ce914914
Notion33 <notion33@gmail.com>
2017-12-07 20:37:46 +0900

    Connected Component 3,4번 (ㅡ,ㄷ자) 에 대해서 완성. / 복잡한구조 linefitting 아직안됌

--- SplitLine.m
+++ SplitLine.m
@@ -1,4 +1,6 @@
-function stats = SplitLine(max_point_index)
+function stats = SplitLine(cc, current_line, max_point_index)
+% SplitLine(4, 1, 1716)
+% SplitLine(2, 1, 823)
 %% Recursively Split line 
 
 %% ㄱ. Initializing
@@ -6,42 +8,52 @@ function stats = SplitLine(max_point_index)
 % load('stats.mat');
 
 global stats
-cc = 4
+threshold_dist = 10;
 
 %% ㄴ. line AC, CB split
 
 % 점 둘로 나누기
-stats(cc).LineList.EndPoint(2, 1:2) = stats(cc).LineList.EndPoint(1, 3:4);
-stats(cc).LineList.EndPoint(1, 3:4) = stats(cc).PixelList(max_point_index,1:2);
-stats(cc).LineList.EndPoint(2, 3:4) = stats(cc).PixelList(max_point_index,1:2);
+stats(cc).Line(current_line).LineList.EndPoint(2, 1:2) = stats(cc).Line(current_line).LineList.EndPoint(1, 3:4);
+stats(cc).Line(current_line).LineList.EndPoint(1, 3:4) = stats(cc).Line(current_line).PixelList(max_point_index,1:2);
+stats(cc).Line(current_line).LineList.EndPoint(2, 3:4) = stats(cc).Line(current_line).PixelList(max_point_index,1:2);
 
 
 % 첫번째 선분 기울기 계산
-a = stats(cc).LineList.EndPoint(1,1);
-b = stats(cc).LineList.EndPoint(1,2);
-c = stats(cc).LineList.EndPoint(1,3);
-d = stats(cc).LineList.EndPoint(1,4);
+a = stats(cc).Line(current_line).LineList.EndPoint(1,1);
+b = stats(cc).Line(current_line).LineList.EndPoint(1,2);
+c = stats(cc).Line(current_line).LineList.EndPoint(1,3);
+d = stats(cc).Line(current_line).LineList.EndPoint(1,4);
 
-A = (d-b)/(c-a);
+if c==a
+	A = 0; 
+else
+    A = (d-b)/(c-a);
+end
 B = b - A*a;
 
-stats(cc).LineList.Line(1,:) = [A B];
+stats(cc).Line(current_line).LineList.Line(1,:) = [A B];
 
+
+% 디버깅용
 x = stats(cc).PixelList(:,1);
 y = stats(cc).PixelList(:,2);
 scatter(x,y,'filled'); grid on; hold on
 plot(x,B+A*x,'r','LineWidth',2);
 
 % 두번째 선분 기울기 계산
-a = stats(cc).LineList.EndPoint(2,1);
-b = stats(cc).LineList.EndPoint(2,2);
-c = stats(cc).LineList.EndPoint(2,3);
-d = stats(cc).LineList.EndPoint(2,4);
+a = stats(cc).Line(current_line).LineList.EndPoint(2,1);
+b = stats(cc).Line(current_line).LineList.EndPoint(2,2);
+c = stats(cc).Line(current_line).LineList.EndPoint(2,3);
+d = stats(cc).Line(current_line).LineList.EndPoint(2,4);
 
-A = (d-b)/(c-a);
+if c==a
+	A = 0; 
+else
+    A = (d-b)/(c-a);
+end
 B = b - A*a;
 
-stats(cc).LineList.Line(2,:) = [A B];
+stats(cc).Line(current_line).LineList.Line(2,:) = [A B];
 
 plot(x,B+A*x,'r','LineWidth',2);
 
@@ -51,27 +63,64 @@ plot(x,B+A*x,'r','LineWidth',2);
 statsA = [];
 statsB = [];
 
-for i=1 : length(stats(cc).PixelList)
-    stats(cc).PixelList(1,3) = abs(stats(cc).LineList.Line(1,2)+stats(cc).LineList.Line(1,1)*stats(cc).PixelList(1,1)-stats(cc).PixelList(1,2))/sqrt(1+stats(cc).LineList.Line(1,1)*stats(cc).LineList.Line(1,1));
-    stats(cc).PixelList(1,4) = abs(stats(cc).LineList.Line(2,2)+stats(cc).LineList.Line(2,1)*stats(cc).PixelList(1,1)-stats(cc).PixelList(1,2))/sqrt(1+stats(cc).LineList.Line(2,1)*stats(cc).LineList.Line(2,1));
-    [~,min_line_index] = min(stats(cc).PixelList(1,3:4));
+for i = 1 : length(stats(cc).Line(current_line).PixelList)
+    A1 = stats(cc).Line(current_line).LineList.Line(1,1);
+    B1 = stats(cc).Line(current_line).LineList.Line(1,2);
+    A2 = stats(cc).Line(current_line).LineList.Line(2,1);
+    B2 = stats(cc).Line(current_line).LineList.Line(2,2);
+    x = stats(cc).Line(current_line).PixelList(1,1);
+    y = stats(cc).Line(current_line).PixelList(1,2);
+    stats(cc).Line(current_line).PixelList(1,3) = abs(B1+A1*x-y)/sqrt(1+A1*A1);
+    stats(cc).Line(current_line).PixelList(1,4) = abs(B2+A2*x-y)/sqrt(1+A2*A2);
+    [~,min_line_index] = min(stats(cc).Line(current_line).PixelList(1,3:4));
     if min_line_index == 1      % 첫번째 라인에 할당된 경우
-        statsA = [statsA; stats(cc).PixelList(1,1:2)];
-    else
-        statsB = [statsB; stats(cc).PixelList(1,1:2)];
+        statsA = [statsA; stats(cc).Line(current_line).PixelList(1,1:2)];
+    else                        % 두번째 라인에 할당된 경우
+        statsB = [statsB; stats(cc).Line(current_line).PixelList(1,1:2)];
     end
-    stats(cc).PixelList(1,:) = [];
+    stats(cc).Line(current_line).PixelList(1,:) = [];
 end
 
 % stats 새로운 point 추가
 
-stats(cc).PixelList = statsA;
-stats(length(stats)+1).PixelList = statsB;
+new_line = length(stats(cc).Line)+1;
+
+stats(cc).Line(current_line).PixelList = statsA;
+stats(cc).Line(current_line).PixelList(:,3) = zeros(length(stats(cc).Line(current_line).PixelList(1)),1);
+stats(cc).Line(current_line).PixelList(:,4) = zeros(length(stats(cc).Line(current_line).PixelList(1)),1);
+stats(cc).Line(new_line).PixelList = statsB;
+stats(cc).Line(new_line).PixelList(:,3) = zeros(length(stats(cc).Line(new_line).PixelList(1)),1);
+stats(cc).Line(new_line).PixelList(:,4) = zeros(length(stats(cc).Line(new_line).PixelList(1)),1);
 
 % line list 갱신
-stats(length(stats)).LineList.Line(1,:) = stats(cc).LineList.Line(2,:);
-stats(cc).LineList.Line(2,:) = [];
-stats(length(stats)).LineList.EndPoint(1,:) = stats(cc).LineList.EndPoint(2,:);
-stats(cc).LineList.EndPoint(2,:) = [];
+stats(cc).Line(new_line).LineList.Line(1,:) = stats(cc).Line(current_line).LineList.Line(2,:);
+stats(cc).Line(current_line).LineList.Line(2,:) = [];
+stats(cc).Line(new_line).LineList.EndPoint(1,:) = stats(cc).Line(current_line).LineList.EndPoint(2,:);
+stats(cc).Line(current_line).LineList.EndPoint(2,:) = [];
 
-[max_dist,max_point_index] = max(stats(cc).PixelList(:,3));
+% if 가장먼거리점 거리> threshold이상 일경우 recursive split
+A = stats(cc).Line(current_line).LineList.Line(1,1);
+B = stats(cc).Line(current_line).LineList.Line(1,2);
+for i=1 : length(stats(cc).Line(current_line).PixelList)
+    x = stats(cc).Line(current_line).PixelList(i,1);
+    y = stats(cc).Line(current_line).PixelList(i,2);
+    stats(cc).Line(current_line).PixelList(i,3) = abs(B+A*x-y)/sqrt(1+A*A);
+end
+[max_dist,max_point_index] = max(stats(cc).Line(current_line).PixelList(:,3));
+if max_dist > threshold_dist
+    SplitLine(cc, current_line, max_point_index);
+end
+
+A = stats(cc).Line(new_line).LineList.Line(1,1);
+B = stats(cc).Line(new_line).LineList.Line(1,2);
+for i=1 : length(stats(cc).Line(new_line).PixelList)
+    x = stats(cc).Line(new_line).PixelList(i,1);
+    y = stats(cc).Line(new_line).PixelList(i,2);
+    stats(cc).Line(new_line).PixelList(i,3) = abs(B+A*x-y)/sqrt(1+A*A);
+end
+[max_dist,max_point_index] = max(stats(cc).Line(new_line).PixelList(:,3));
+if max_dist > threshold_dist
+    SplitLine(cc, new_line, max_point_index);
+end
+
+end
--- splitnmerge.m
+++ splitnmerge.m
@@ -4,51 +4,55 @@
 load('SplitNMergeVariable.mat');
 
 global stats
-cc = 4;
+cc = 2;
 threshold_dist = 10;
 
+%% Initializing, 각 connected component 별 point, start&end point, line 할당 
 
-%% 1. initializing Starting point 이용해서 초기선분 잡기
+for i=1 : length(stats)
+    stats(i).Line.PixelList = stats(i).PixelList;
 
-a = global_max_point.location(cc,1);
-b = global_max_point.location(cc,2);
-c = global_max_point.location(cc,3);
-d = global_max_point.location(cc,4);
-
-A = (d-b)/(c-a);
-B = b - A*a;
-
-x = stats(cc).PixelList(:,1);
-y = stats(cc).PixelList(:,2);
-
-[stats(cc).LineList.Line] = [A B];
-[stats(cc).LineList.EndPoint] = [a b c d];
+    a = global_max_point.location(cc,1);
+    b = global_max_point.location(cc,2);
+    c = global_max_point.location(cc,3);
+    d = global_max_point.location(cc,4);
+    
+    if c==a
+        A = 0; 
+    else
+        A = (d-b)/(c-a);
+    end
+    B = b - A*a;
+    [stats(i).Line.LineList.Line] = [A B];
+    [stats(i).Line.LineList.EndPoint] = [a b c d];
+    
+    stats(i).Line.PixelList(:,3) = zeros(length(stats(i).Line.PixelList(1)),1);
+    stats(i).Line.PixelList(:,4) = zeros(length(stats(i).Line.PixelList(1)),1);
+end
 
 
 %% 2. if 가장먼거리점 거리> threshold이상 일경우 split
 
-stats(cc).PixelList(:,3) = zeros(length(stats(cc).PixelList(1)),1);
-stats(cc).PixelList(:,4) = zeros(length(stats(cc).PixelList(1)),1);
-for i=1 : length(stats(cc).PixelList)
-    stats(cc).PixelList(i,3) = abs(B+A*stats(cc).PixelList(i,1)-stats(cc).PixelList(i,2))/sqrt(1+A*A);
+for i=1 : length(stats(cc).Line.PixelList)
+    stats(cc).Line.PixelList(i,3) = abs(B+A*stats(cc).Line.PixelList(i,1)-stats(cc).Line.PixelList(i,2))/sqrt(1+A*A);
 end
-[max_dist,max_point_index] = max(stats(cc).PixelList(:,3));
+[max_dist,max_point_index] = max(stats(cc).Line.PixelList(:,3));
 
 if max_dist > threshold_dist
-    stats.(cc) = SplitLine(max_point_index);
+    SplitLine(max_point_index);
 end
 
 %% debugging 용 이미지제작용
 
-scatter(x,y,'filled'); grid on; hold on
-plot(x,B+A*x,'r','LineWidth',2);
-
-r = 4;
-xc = stats(cc).PixelList(max_point_index,1);
-yc = stats(cc).PixelList(max_point_index,2);
-
-theta = linspace(0,2*pi);
-x = r*cos(theta) + xc;
-y = r*sin(theta) + yc;
-plot(x,y)
-axis equal
+% scatter(x,y,'filled'); grid on; hold on
+% plot(x,B+A*x,'r','LineWidth',2);
+% 
+% r = 4;
+% xc = stats(cc).PixelList(max_point_index,1);
+% yc = stats(cc).PixelList(max_point_index,2);
+% 
+% theta = linspace(0,2*pi);
+% x = r*cos(theta) + xc;
+% y = r*sin(theta) + yc;
+% plot(x,y)
+% axis equal
